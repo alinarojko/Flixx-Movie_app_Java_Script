0ebf0c9355c3f321758b38bed201b06990f05d0c
ArthurProgrammer1 <rojko.alina@gmail.com>
2025-07-02 17:17:24 +0100

    Slider on the main page

--- flixx-app/js/script.js
+++ flixx-app/js/script.js
@@ -245,7 +245,57 @@ async function fetchAPIData(endpoint) {
 
   hideSpinner();
   return data;
-}
+};
+
+// Display slider movies 
+async function displaySlider(){
+  const { results } = await fetchAPIData('movie/now_playing');
+  results.forEach((movie) => {
+    const div = document.createElement('div');
+    div.classList.add('swiper-slide');
+
+    div.innerHTML = `
+      <a href="movie-details.html?id=${movie.id}">
+        <img src="https://image.tmdb.org/t/p/w500${movie.poster_path}" alt="${movie.title}" />
+      </a>
+      <h4 class="swiper-rating">
+        <i class="fas fa-star text-secondary"></i> ${movie.vote_average} / 10
+      </h4>
+    `;
+
+    document.querySelector('.swiper-wrapper').appendChild(div);
+
+    initSwiper();
+
+  });
+
+
+function initSwiper() {
+  const swiper = new Swiper('.swiper', {
+    slidesPerView: 1, 
+    spaceBetween: 30,
+    freeMode: true,
+    autoplay: {
+      delay: 4000,
+      disableOnInteraction: false,
+    },
+    breakpoints: {
+      500: {
+        slidesPerView: 2,
+        spaceBetween: 20,
+      },
+      700: {
+        slidesPerView: 3,
+        spaceBetween: 30,
+      },
+      1200: {
+        slidesPerView: 4,
+        spaceBetween: 40,
+      },
+    }
+  }
+
+)};
 
 // Show spinner
 function showSpinner(){
@@ -278,8 +328,9 @@ function addCommasToNumber(number){
 function init() {
   switch (global.currentPage) {
     case '/':
-    case '/flixx-app/index.html':
+    case '/flixx-app/':
       displayPopularMovies();
+      displaySlider()
       break;
     case '/shows.html':
       displayPopularShows();
